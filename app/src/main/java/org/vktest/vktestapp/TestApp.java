115f989f90621b6c0565345cd62e4bf1d971df30
Paul Savchenko <pavel.savchenko@sharp-dev.net>
2018-06-27 15:23:12 +1000

    Leak Canary added

--- app/src/main/java/org/vktest/vktestapp/TestApp.java
+++ app/src/main/java/org/vktest/vktestapp/TestApp.java
@@ -2,6 +2,8 @@ package org.vktest.vktestapp;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import org.vktest.vktestapp.di.AppComponent;
 import org.vktest.vktestapp.di.DaggerAppComponent;
 import org.vktest.vktestapp.di.modules.BaseModule;
@@ -15,6 +17,12 @@ public class TestApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
         sAppComponent = DaggerAppComponent.builder()
                 .baseModule(new BaseModule(this))
